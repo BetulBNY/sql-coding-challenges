bc270392308ab8278f4a4275c1f0af910b615478
BetulBNY <betulbny@gmail.com>
2026-04-29 18:28:45 +0300

    Leetcode Inner Join Question (Employees Earning More Than Their Managers)

--- day03_27-04-2026-181_Employees_Earning_More_Thane_Their_Managers.sql
+++ day03_27-04-2026-181_Employees_Earning_More_Thane_Their_Managers.sql
@@ -15,10 +15,10 @@ Each row of this table indicates the ID of an employee, their name, salary, and 
 Write a solution to find the employees who earn more than their managers.
 Return the result table in any order.
 */
+-- SELF JOIN SOLUTION
 SELECT 
     e.name AS Employee
 FROM Employee AS e
 JOIN Employee AS m
     ON e.managerId = m.id
 WHERE e.salary > m.salary;
--- SELF JOIN SOLUTION
